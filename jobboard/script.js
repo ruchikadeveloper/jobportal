549ad3f654c6738d209e0e4e2ef060a2db2f996e
Ruchika Shakya <139382679+ruchikadeveloper@users.noreply.github.com>
2023-08-01 18:24:43 +0530

    Update script.js

--- jobboard/script.js
+++ jobboard/script.js
@@ -29,32 +29,161 @@ $(".navbar-menu a").click(function(e){
 
 
 
-// var swiper = new Swiper(".mySwiper",
-// {
-//     loop:true,
-//     autoplay: {
-//         delay:2500,
-//         disableOnInteraction:false,
-// },
-// slidesPerView:1,
-// spaceBetween:10,
-// pagination:{
-//     el:".swiper-pagination",
-//     clickable:true,
-// },
-// breakpoints:{
-//     640:{
-//         slidesPerView:2,
-//         spaceBetween:20,
-//     },
-//     768:{
-//         slidesPerView:3,
-//         spaceBetween:30, 
-//     },
-//     1024:{
-//         slidesPerView:4,
-//         spaceBetween:40,
-//     }
-// }
-// }
-// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to open signup modal
+function openSignupModal() {
+    var modal = document.getElementById("signupModal");
+    modal.style.display = "block";
+}
+
+// Function to close signup modal
+function closeSignupModal() {
+    var modal = document.getElementById("signupModal");
+    modal.style.display = "none";
+}
+
+// Function to open login modal
+function openLoginModal() {
+    var modal = document.getElementById("loginModal");
+    modal.style.display = "block";
+}
+
+// Function to close login modal
+function closeLoginModal() {
+    var modal = document.getElementById("loginModal");
+    modal.style.display = "none";
+}
+
+// Function to handle signup form submission
+function handleSignupFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Get form input values
+    var username = document.getElementById("signupUsername").value;
+    var email = document.getElementById("signupEmail").value;
+    var password = document.getElementById("signupPassword").value;
+    var phone = document.getElementById("signupPhoneNumber").value;
+    var dob = document.getElementById("signupDOB").value;
+
+    // Perform basic form validation (you can add more validation as needed)
+    if (!username || !email || !password || !phone || !dob) {
+        alert("Please fill in all the required fields.");
+        return;
+    }
+
+    // Show a success message (in a real app, you would send data to the server here)
+    alert("Signup successful! Welcome, " + username + "!");
+    closeSignupModal();
+
+    // Clear the form fields (optional, you can remove this if you don't want to clear the form)
+    document.getElementById("signupUsername").value = "";
+    document.getElementById("signupEmail").value = "";
+    document.getElementById("signupPassword").value = "";
+    document.getElementById("signupPhoneNumber").value = "";
+    document.getElementById("signupDOB").value = "";
+}
+
+// Function to handle login form submission
+function handleLoginFormSubmit(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Get form input values
+    var email = document.getElementById("loginEmail").value;
+    var password = document.getElementById("loginPassword").value;
+
+    // Perform basic form validation (you can add more validation as needed)
+    if (!email || !password) {
+        alert("Please fill in all the required fields.");
+        return;
+    }
+
+    // Show a success message (in a real app, you would send data to the server here)
+    alert("Login successful! Welcome back!");
+    closeLoginModal();
+
+    // Clear the form fields (optional, you can remove this if you don't want to clear the form)
+    document.getElementById("loginEmail").value = "";
+    document.getElementById("loginPassword").value = "";
+}
+
+// Add event listeners to the signup and login forms
+var signupForm = document.getElementById("signupForm");
+var loginForm = document.getElementById("loginForm");
+signupForm.addEventListener("submit", handleSignupFormSubmit);
+loginForm.addEventListener("submit", handleLoginFormSubmit);
+
+
+
+
+
+
+
+// Function to handle search
+function handleSearch(event) {
+    // Check if the "Enter" key is pressed (key code 13)
+    if (event.keyCode === 13) {
+        // Get the search input value
+        var searchInput = document.querySelector(".search-input").value;
+
+        // Perform the search logic here (for demonstration purposes, we'll just show an alert with the search query)
+        if (searchInput.trim() === "") {
+            alert("Please enter a search keyword.");
+            return;
+        } else {
+            alert("Performing search for: " + searchInput);
+            // You can add the actual search logic here, such as making an API call or filtering results on the page.
+        }
+    }
+}
+
+// Add event listener to the search input field for the "keydown" event
+var searchInputField = document.querySelector(".search-input");
+searchInputField.addEventListener("keydown", handleSearch);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
